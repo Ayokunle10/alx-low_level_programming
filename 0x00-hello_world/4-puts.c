960156a9e4db8cf153b8b4f69c58d2277f4d58f3
Ayokunle10 <martinsayokunle897@gmail.com>
2022-07-01 18:09:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main -> this is a function that prints hello, puts
+ * Return: sucess always 0
+ */
 int main(void)
-{
-printf("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
